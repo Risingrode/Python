a09dd21f096799963992a91adfdad1d154b7b3f6
代码有点萌 <97512230+Risingrode@users.noreply.github.com>
2024-07-14 21:01:01 +0800

    Delete Python-111-main directory

--- Python-111-main/巨潮资讯股票数据获取/123.js
+++
@@ -1,8 +0,0 @@
-var indexcode={
-    getResCode:function(){
-//      时间戳
-        var time=Math.floor(new Date().getTime()/1000);
-//        调用了某一个方法 ""+time
-        return window.JSonToCSV.missjson(""+time);
-    }
-}
